1abab6e3e1a6f5a12704ff480b137a54614ff1fe
AggelosPsimitis <60297815+AggelosPsimitis@users.noreply.github.com>
2022-04-21 20:55:34 +0300

    Update settings.py

--- yelp_python/yelp_python/settings.py
+++ yelp_python/yelp_python/settings.py
@@ -1,7 +1,7 @@
 # MySQL Settings
-mysql_host =  '127.0.0.1' 
-mysql_port = 3306  # must be integer (this is wrong:'3306')
-mysql_user = 'root'   #must replace with your own settings
+mysql_host =  '' 
+mysql_port =   # must be integer (this is wrong:'3306')
+mysql_user = ''   #must replace with your own settings
 mysql_passwd = '' #must replace with your own settings 
 mysql_schema = 'yelp'
 
